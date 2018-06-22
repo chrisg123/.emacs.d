239bc40e25c5753364f725379f40dcd0d12da364
chrisg123 <chrisg_123@hotmail.com>
2018-06-22 18:39:05 -0400

    minor changes

--- custom/emacs-customize.el
+++ custom/emacs-customize.el
@@ -1,3 +1,7 @@
+;; Set path to external file for use by the emacs customize system.
+;; https://stackoverflow.com/a/5052111
+(setq custom-file "~/.emacs.d/custom/emacs-customize.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -11,3 +15,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'emacs-customize)
--- init.el
+++ init.el
@@ -1,5 +1,6 @@
-;; Add MELPA to package-archives. (https://www.emacswiki.org/emacs/MELPA)
 (require 'package)
+
+;; Add MELPA to package-archives. (https://www.emacswiki.org/emacs/MELPA)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 ;; Avoid loading the packages again after processing the init file. 
@@ -8,19 +9,16 @@
 ;; Load packages explicitly.
 (package-initialize)
 
-;; Set path to external file for use by the emacs customize system.
-;; https://stackoverflow.com/a/5052111
-(setq custom-file "~/.emacs.d/custom/emacs-customize.el")
-(load custom-file)
-
 ;; Ensure all selected packages are installed.
 ;; https://stackoverflow.com/a/39891192/2974621
+;; Note: (package-selected-packages) is declared in (custom-file).
 (unless package-archive-contents
   (package-refresh-contents))
 (package-install-selected-packages)
 
 ;; Load custom scripts.
 (add-to-list 'load-path "~/.emacs.d/custom/")
+(require 'emacs-customize)
 (require 'emacs-saves)
 (require 'line-numbers)
 (require 'move-line)
