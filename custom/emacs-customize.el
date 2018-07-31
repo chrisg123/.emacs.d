30e3d636d320fc8542ad1951180c2c1aa1604b91
chrisg123 <chrisg_123@hotmail.com>
2018-07-30 22:35:39 -0400

    updates

--- custom/emacs-customize.el
+++ custom/emacs-customize.el
@@ -24,4 +24,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :foreground "WhiteSmoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
+ '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :foreground "WhiteSmoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
+ '(org-document-title ((t (:foreground "brightyellow" :weight bold)))))
