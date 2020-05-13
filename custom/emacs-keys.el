ae7db26d8d9c07cfed9f5de56740ce0335747c83
cgouveia <dev@CG-DEV1.orsolutions.com>
2020-05-13 09:22:28 -0400

    add keys to move windows around.

--- custom/emacs-keys.el
+++ custom/emacs-keys.el
@@ -5,6 +5,11 @@
 
 (define-key global-map (kbd "C-c s") 'insert-source-block)
 
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+
 (provide 'emacs-keys)
 
 ;;; emacs-keys.el ends here
