be3de1b679520594b73e3467a6408e6642d7f75a
chrisg123 <chrisg_123@hotmail.com>
2021-01-07 17:59:06 -0500

    customizations

--- custom/emacs-customize.el
+++ custom/emacs-customize.el
@@ -23,7 +23,7 @@
  '(global-semantic-idle-scheduler-mode t)
  '(global-semanticdb-minor-mode t)
  '(package-selected-packages
-   '(rust-mode flycheck company-jedi elpy flycheck-pycheckers python-mode xah-math-input multiple-cursors org-projectile flycheck-kotlin lsp-ui yaml-mode auto-complete company ghc groovy-mode magit company-lsp flycheck-gradle gradle-mode kotlin-mode lsp-mode markdown-mode format-all android-mode cmake-mode geben-helm-projectile geben highlight-indent-guides company-nginx helm-c-yasnippet ac-php company-php nginx-mode hindent ac-haskell-process haskell-snippets flycheck-ghcmod company-ghc helm))
+   '(linum-relative rust-mode flycheck company-jedi elpy flycheck-pycheckers python-mode xah-math-input multiple-cursors org-projectile flycheck-kotlin lsp-ui yaml-mode auto-complete company ghc groovy-mode magit company-lsp flycheck-gradle gradle-mode kotlin-mode lsp-mode markdown-mode format-all android-mode cmake-mode geben-helm-projectile geben highlight-indent-guides company-nginx helm-c-yasnippet ac-php company-php nginx-mode hindent ac-haskell-process haskell-snippets flycheck-ghcmod company-ghc helm))
  '(safe-local-variable-values '((c-indent-level . 4)))
  '(yaml-indent-offset 4))
 (custom-set-faces
@@ -33,9 +33,13 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "unspecified-bg" :foreground "WhiteSmoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
  '(diff-header ((t (:background "brightblack"))))
+ '(ediff-even-diff-A ((t (:extend t :background "color-245"))))
+ '(ediff-even-diff-B ((t (:extend t :background "brightblack"))))
+ '(ediff-odd-diff-A ((t (:extend t :background "brightblack"))))
  '(font-lock-keyword-face ((t (:foreground "cyan"))))
  '(helm-selection ((t (:background "color-238" :distant-foreground "black"))))
  '(helm-visible-mark ((t (:background "color-19"))))
+ '(highlight-indentation-face ((t (:inherit nil))))
  '(lsp-ui-peek-list ((t (:background "brightblack"))))
  '(lsp-ui-peek-peek ((t (:background "brightblack"))))
  '(magit-diff-added ((t (:foreground "#22aa22"))))
@@ -53,4 +57,6 @@
  '(pulse-highlight-start-face ((t (:background "color-19"))))
  '(semantic-decoration-on-fileless-includes ((t (:background "#f0fdf0" :foreground "black"))))
  '(semantic-decoration-on-unparsed-includes ((t (:background "#ffff55" :foreground "black"))))
- '(semantic-highlight-func-current-tag-face ((t (:background "color-19" :foreground "white")))))
+ '(semantic-highlight-func-current-tag-face ((t (:background "color-19" :foreground "white"))))
+ '(whitespace-space ((t (:foreground "#262626"))))
+ '(whitespace-tab ((t (:foreground "#636363")))))
