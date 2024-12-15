3693321dc033ac6f79c0510977420b9fd4fff4ca
chrisg123 <chrisg_123@hotmail.com>
2024-12-14 19:05:39 -0500

    Flycheck for emscripten

--- custom/my-flycheck.el
+++ custom/my-flycheck.el
@@ -1,6 +1,7 @@
 ;;; my-flycheck.el --- My flycheck customization
 ;;; Commentary:
 ;;; Code:
+(require 'flycheck)
 (defvar flycheck-emacs-lisp-load-path)
 (setq flycheck-emacs-lisp-load-path 'inherit)
 
@@ -18,6 +19,26 @@
                                              16 17
                                              (face default))
                                            0 t)])))
+
+(with-eval-after-load 'flycheck
+  (add-to-list 'flycheck-checkers 'c/c++-emscripten))
+
+(defvar flycheck-emscripten-include-path '()
+  "A list of include paths for the Emscripten checker.")
+
+(flycheck-define-checker c/c++-emscripten
+  "A C/C++ checker using Emscripten."
+  :command ("emcc"
+            "-Wall" "-Wextra"
+            "-std=c++17"
+            "-S"  ; Only run preprocesser and compilation steps
+            "-o" "/dev/null"
+            (option-list "-I" flycheck-emscripten-include-path)
+            source)
+  :error-patterns
+  ((warning line-start (file-name) ":" line ":" column ": warning: " (message) line-end)
+   (error line-start (file-name) ":" line ":" column ": " (message) line-end))
+  :modes (c-mode c++-mode))
 
 (provide 'my-flycheck)
 ;;; my-flycheck ends here
